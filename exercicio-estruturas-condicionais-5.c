5f182935a05e0d4811e2b17a477988c65322b90a
Marina Jeronymo <marinajeronymo@Marinas-MacBook-Air.local>
2022-08-20 11:59:03 -0300

    add-arquivos-compilados-e-exercicio-4

---
+++ exercicio-estruturas-condicionais-5.c
@@ -0,0 +1,24 @@
+/*Fazer um programa para receber valores inteiros X, Y e Z do usuário e determinar se estes valores podem formar os lados de um triângulo. Em caso afirmativo, informar se o triângulo é equilátero, isósceles ou escaleno.*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int lado1, lado2, lado3;
+
+    printf("Escreva um número");
+    scanf("%d",&lado1);
+
+    printf("Escreva um número");
+    scanf("%d",&lado2);
+
+    printf("Escreva um número");
+    scanf("%d",&lado3);
+
+    if()
+    
+    
+
+
+    return 0;
+}
