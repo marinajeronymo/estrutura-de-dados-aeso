1c6d8d38d4516b7006a985cdf48487a685ab9199
Marina Jeronymo <marinajeronymo@Marinas-MacBook-Air.local>
2022-08-26 11:25:14 -0300

    add-resolucao-exercicio-6

--- exercicio-estruturas-condicionais-5.c
+++ exercicio-estruturas-condicionais-5.c
@@ -6,18 +6,27 @@ int main(int argc, char const *argv[])
 {
     int lado1, lado2, lado3;
 
-    printf("Escreva um número");
+    printf("Escreva um número: ");
     scanf("%d",&lado1);
 
-    printf("Escreva um número");
+    printf("\nEscreva um número: ");
     scanf("%d",&lado2);
 
-    printf("Escreva um número");
+    printf("\nEscreva um número: ");
     scanf("%d",&lado3);
 
-    if()
-    
-    
+    if(lado1<(lado2+lado3)&&lado2<(lado3+lado1)&&lado3<lado1+lado2){
+        if((lado1==lado2)&&(lado1==lado3)){
+            printf("Triângulo Equilátero");
+        }else if((lado1==lado2)&&(lado1!=lado3)||(lado1==lado3)&&(lado1!=lado2)||(lado2==lado3)&&(lado2!=lado1)){
+            printf("Triângulo Isósceles");
+        }else if((lado1!=lado2)&&(lado2!=lado3)&&(lado1!=lado3)){
+            printf("Triângulo Escaleno");
+        }
+
+    }else{
+        printf("\n Não é um triângulo");
+    }
 
 
     return 0;
